c44cbf0a8db3c99d8aea14cf24b550d98002bba8
Shohruz Bobobekov <shohruzbobobekov505@gmail.com>
2023-01-22 16:48:08 +0500

    Added ExamApplicant model

---
+++ BulungurAcademy.Domain/BulungurAcademy.Domain/Entities/ExamApplicants/ExamApplicant.cs
@@ -0,0 +1,18 @@
+﻿using BulungurAcademy.Domain.Entities.Common;
+using BulungurAcademy.Domain.Entities.Exams;
+using BulungurAcademy.Domain.Entities.Subjects;
+using BulungurAcademy.Domain.Entities.Users;
+
+namespace BulungurAcademy.Domain.Entities;
+
+public class ExamApplicant : Auditable
+{
+    public Guid UserId { get; set; }
+    public Guid ExamId { get; set; }
+    public Guid FirstSubjectId { get; set; }
+    public Guid SecondSubjectId { get; set;}
+    public User User { get; set; }
+    public Exam Exam { get; set; }
+    public Subject FirstSubject { get; set; }
+    public Subject SecondSubject { get; set; }
+}
