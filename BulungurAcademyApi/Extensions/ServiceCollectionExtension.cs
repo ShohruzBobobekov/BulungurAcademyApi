4b8415c29733557b079028d085b73b73f68b6434
Shohruz Bobobekov <shohruzbobobekov505@gmail.com>
2023-01-29 15:27:30 +0500

    DbContext Added DI Container and Clean Up Project

---
+++ BulungurAcademyApi/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using BulungurAcademy.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulungurAcademy.Api.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddDbContexts(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString=configuration.GetConnectionString("SqlServer");
+
+        services.AddDbContextPool<AppDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString, sqlServerOptions =>
+            {
+                sqlServerOptions.EnableRetryOnFailure();
+            });
+        });
+
+        return services;
+    }
+}
