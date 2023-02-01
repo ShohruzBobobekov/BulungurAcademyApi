ff69a8aba1b18a61bf6e52788e4506d47922b85f
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-02 03:07:40 +0500

    added services for subject entity

---
+++ BulungurAcademy.Application/BulungurAcademy.Application/Services/Subjects/ISubjectService.cs
@@ -0,0 +1,11 @@
+﻿using BulungurAcademy.Application.DataTranferObjects;
+
+namespace BulungurAcademy.Application.Services;
+public interface ISubjectService
+{
+    ValueTask<SubjectDto> CreateUserAsync(SubjectForCreationDto subjectForCreationDto);
+    IQueryable<SubjectDto> RetrieveUsers(/*QueryParameter queryParameter*/);
+    ValueTask<SubjectDto> RetrieveUserByIdAsync(Guid subjectId);
+    ValueTask<SubjectDto> ModifyUserAsync(SubjectForModificationDto subjectForModificationDto);
+    ValueTask<SubjectDto> RemoveUserAsync(Guid subrectId);
+}
