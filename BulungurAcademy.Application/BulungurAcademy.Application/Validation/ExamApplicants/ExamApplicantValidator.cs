296dc96e5fd466d812b17f51d84e7f635c5bce88
Shohruz Bobobekov <shohruzbobobekov505@gmail.com>
2023-02-09 23:58:41 +0500

    Added ExamApplicant Service and validator

---
+++ BulungurAcademy.Application/BulungurAcademy.Application/Validation/ExamApplicants/ExamApplicantValidator.cs
@@ -0,0 +1,29 @@
+﻿using BulungurAcademy.Domain.Entities;
+using FluentValidation;
+
+namespace BulungurAcademy.Application.Validation.ExamApplicants;
+
+public class ExamApplicantValidator : AbstractValidator<ExamApplicant>
+{
+    public ExamApplicantValidator()
+    {
+        RuleFor(examApplicant => examApplicant)
+            .NotNull();
+
+        RuleFor(examApplicant => examApplicant.ExamId)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(examApplicant => examApplicant.UserId)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(examApplicant => examApplicant.FirstSubjectId)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(examApplicant => examApplicant.SecondSubjectId)
+            .NotNull()
+            .NotEmpty();
+    }
+}
