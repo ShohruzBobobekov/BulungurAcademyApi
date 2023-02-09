6206ec4228656f3c7d7b7d40ff803e15e6abddfc
diyorbek2506@gmail.com <diyorbek2506@gmail.com>
2023-02-09 17:28:04 +0500

    Initialize Exam Service

---
+++ BulungurAcademy.Application/BulungurAcademy.Application/Services/Exams/IExamService.cs
@@ -0,0 +1,13 @@
+﻿using BulungurAcademy.Domain.Entities.Exams;
+
+namespace BulungurAcademy.Application.Services.Exams;
+
+public interface IExamService
+{
+    IQueryable<Exam> RetrieveExams();
+    ValueTask<Exam> RetrieveExamByIdAsync(Guid id);
+    ValueTask<Exam> RetrieveExamWithDetailsAsync(Guid id);
+    ValueTask<Exam> CreateExamAsync(Exam exam);
+    ValueTask<Exam> ModifyExamAsync(Exam exam);
+    ValueTask<Exam> RemoveExamAsync(Guid id);
+}
