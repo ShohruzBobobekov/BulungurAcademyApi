4973119af2b82d8952f2442447cd1deb10ac027f
Shohruz Bobobekov <shohruzbobobekov505@gmail.com>
2023-02-09 23:57:55 +0500

    Added SelectByIdWithDetailsAsync method in ExamApplicants repository

--- BulungurAcademy.Infrastructure/BulungurAcademy.Infrastructure/Repositories/ExamApplicants/IExamApplicantRepository.cs
+++ BulungurAcademy.Infrastructure/BulungurAcademy.Infrastructure/Repositories/ExamApplicants/IExamApplicantRepository.cs
@@ -4,5 +4,12 @@ namespace BulungurAcademy.Infrastructure.Repositories.ExamApplicants;
 
 public interface IExamApplicantRepository : IRepository<ExamApplicant>
 {
-
+    /// <summary>
+    /// examId va userId bo'yicha ExamApplicant ro'yhatini yuklab oladi.
+    /// examId va userId Compozit key.
+    /// </summary>
+    /// <param name="examId"></param>
+    /// <param name="userId"></param>
+    /// <returns name="ExamApplicant"></returns>
+    ValueTask<ExamApplicant> SelectByIdWithDetailsAsync(Guid examId, Guid userId);
 }
