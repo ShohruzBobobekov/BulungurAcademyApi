e2e575a8c9149796950c0fa3ddf4f3035b74849b
Muxammadyor <muxammadyorkenjayev21@gmail.com>
2023-02-11 18:32:57 +0500

    Controller yozildi

--- BulungurAcademyApi/Controllers/UserController.cs
+++ BulungurAcademyApi/Controllers/UserController.cs
@@ -1,9 +1,73 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulungurAcademy.Application.DataTranferObjects.Users;
+using BulungurAcademy.Application.Services.Users;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulungurAcademy.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class UserController : Controller
+
+public class UserController : ControllerBase
 {
+    private readonly IUserService userService;
+
+    public UserController(
+        IUserService userService)
+    {
+        this.userService = userService;
+    }
+
+    [HttpPost]
+    public async ValueTask<ActionResult<UserDto>> PostUserAsync(
+           UserForCreaterDto userForCreationDto)
+    {
+        var createdUser = await this.userService
+            .CreateUserAsync(userForCreationDto);
+
+        return Created("", createdUser);
+    }
+
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        var users = this.userService
+            .RetrieveUsers();
+
+        return Ok(users);
+    }
+
+
+    [HttpGet("{userId:guid}")]
+    public async ValueTask<ActionResult<UserDto>> GetUserByIdAsync(
+            Guid userId)
+    {
+        var user = await this.userService
+            .RetrieveUserByIdAsync(userId);
+
+        return Ok(user);
+    }
+
+    [HttpPut]
+    public async ValueTask<ActionResult<UserDto>> PutUserAsync(
+            UserForModificationDto userForModificationDto)
+    {
+        var modifiedUser = await this.userService
+            .ModifyUserAsync(userForModificationDto);
+
+        return Ok(modifiedUser);
+    }
+
+
+
+    [HttpDelete("{userId:guid}")]
+    public async ValueTask<ActionResult<UserDto>> DeleteUserAsync(Guid userId)
+    {
+        var removed = await this.userService
+                .RemoveUserAsync(userId);
+
+        
+
+        return Ok(removed);
+    }
+
 }
