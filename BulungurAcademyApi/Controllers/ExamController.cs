1a23e7efab196e45c4d9c86e9b1e6455a2d6e676
diyorbek2506@gmail.com <diyorbek2506@gmail.com>
2023-02-14 09:14:49 +0500

    Exam Controller Added

---
+++ BulungurAcademyApi/Controllers/ExamController.cs
@@ -0,0 +1,38 @@
+﻿using BulungurAcademy.Application.Services.Exams;
+using BulungurAcademy.Domain.Entities.Exams;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulungurAcademy.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExamController : ControllerBase
+{
+    private readonly IExamService examService;
+
+    public ExamController(IExamService examService)
+    {
+        this.examService = examService;
+    }
+
+    [HttpGet]
+    public IActionResult GetExams()
+    {
+        var exams = examService.RetrieveExams();
+
+        return Ok(exams);
+    }
+
+    [HttpGet("examId:Guid")]
+    public async Task<IActionResult> GetExamByIdAsync(Guid examId)
+    {
+        var exam = await examService.RetrieveExamByIdAsync(id: examId);
+
+        return Ok(exam);
+    }
+
+    [HttpPost] IActionResult CreateExamAsync(Exam exam)
+    {
+
+    }
+}
