f8fa2c90dfbaea1e80459b106b85cdbe3a92ade2
Shohruz Bobobekov <shohruzbobobekov505@gmail.com>
2023-02-14 09:37:47 +0500

    Fix code

--- BulungurAcademy.Application/BulungurAcademy.Application/DataTranferObjects/ExamApplicants/ExamApplicantDto.cs
+++ BulungurAcademy.Application/BulungurAcademy.Application/DataTranferObjects/ExamApplicants/ExamApplicantDto.cs
@@ -1,8 +1,13 @@
-﻿namespace BulungurAcademy.Application.DataTranferObjects.ExamApplicants;
+﻿using BulungurAcademy.Application.DataTranferObjects.Users;
+
+namespace BulungurAcademy.Application.DataTranferObjects.ExamApplicants;
 
 public record ExamApplicantDto(
      Guid UserId,
      Guid ExamId,
      Guid? FirstSubjectId,
-     Guid? SecondSubjectId
+     Guid? SecondSubjectId,
+     UserDto UserDto
+     //SubjectDto FirstSubject,
+     //SubjectDto SecondSubject
 );
