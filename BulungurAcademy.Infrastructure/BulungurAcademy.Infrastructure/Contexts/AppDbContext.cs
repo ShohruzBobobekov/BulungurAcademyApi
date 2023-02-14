202b05d047d5a4a2d5ccd29d72c4a1e5d36c6ab4
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-15 03:33:05 +0500

    nothing

--- BulungurAcademy.Infrastructure/BulungurAcademy.Infrastructure/Contexts/AppDbContext.cs
+++ BulungurAcademy.Infrastructure/BulungurAcademy.Infrastructure/Contexts/AppDbContext.cs
@@ -8,10 +8,7 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Database=BulungurAcademy;");
-    //}
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(
