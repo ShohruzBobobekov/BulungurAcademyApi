1711f5a70f630d2f66f21c1f128e64a59d10e672
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-16 18:01:29 +0500

    exam  bajarildi

--- BulungurAcademy.Domain/BulungurAcademy.Domain/Entities/Exams/Exam.cs
+++ BulungurAcademy.Domain/BulungurAcademy.Domain/Entities/Exams/Exam.cs
@@ -7,6 +7,12 @@ public class Exam : Auditable
 {
     public string? ExamName { get; set; }
     public DateTime ExamDate { get; set; }
+    public Exam() { }
+    public Exam(string? examName, DateTime examDate = default)
+    {
+        ExamName = examName;
+        ExamDate = examDate;
+    }
 
     public ICollection<ExamApplicant>? ExamApplicants { get; set; }
     public ICollection<ExamSubject>? ExamSubjects { get; set; }
