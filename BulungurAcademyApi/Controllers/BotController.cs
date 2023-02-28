eeeaf812b8750bcc1192aef62b51f41bc7b01153
diyorbek2506@gmail.com <diyorbek2506@gmail.com>
2023-02-28 08:45:33 +0500

    added bot controller

---
+++ BulungurAcademyApi/Controllers/BotController.cs
@@ -0,0 +1,20 @@
+﻿using BulungurAcademy.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+
+namespace BulungurAcademy.Core.Controllers;
+
+[ApiController]
+[Route("bot")]
+public class BotController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Update update,
+        [FromServices] UpdateHandler updateHandler)
+    {
+        await updateHandler
+            .UpdateHandlerAsync(update);
+
+        return Ok();
+    }
+}
