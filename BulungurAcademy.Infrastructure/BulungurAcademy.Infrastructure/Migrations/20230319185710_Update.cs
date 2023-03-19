b02d9907c5a4ef0abdb198b9cbab7c074f7c2907
diyorbek2506@gmail.com <diyorbek2506@gmail.com>
2023-03-19 23:59:45 +0500

    added PaymentStatus , AttendenceStatus and Migration

---
+++ BulungurAcademy.Infrastructure/BulungurAcademy.Infrastructure/Migrations/20230319185710_Update.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulungurAcademy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PaymentStatus",
+                table: "ExamApplicant",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AttendanceStatus",
+                table: "ExamApplicant",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PaymentStatus",
+                table: "ExamApplicant",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AttendanceStatus",
+                table: "ExamApplicant",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
